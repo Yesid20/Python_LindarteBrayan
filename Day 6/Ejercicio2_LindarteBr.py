fe36a8d7c6963c897cf47c343411cb76260b6695
Yesid20 <edgarcarloscamilo@gmail.com>
2024-02-04 11:01:43 -0500

    Definiciones teoricas

---
+++ Day 6/Ejercicio2_LindarteBr.py
@@ -0,0 +1,20 @@
+## ----------------------------------
+# ---- Ejercicio 2 ----
+## ---------------------------------
+
+'''
+Estructura De Datos 
+
+* String = Sucesion inalterable de elementos 
+  Mutabilidad = Inmutable.
+  Ejemplo: "Hola soy un programador junior, mamá estoy triunfando"
+* Lista = Almacenan varios elementos en una sola variable
+  Mutabiliad = Mutable.
+  Ejemplo: [1,2,3] .append, .insert, .remove, .pop, .reverse
+* Tupla = Sucesion de elementos dentro de una variable que no se pueden modificar.
+  Mutabiliad = Inmutable 
+  Ejemplo: ("uno","dos","tres")
+* Diccionario = Conjunto de elemenos que se ligan a una Keys y Values respectivamente
+  Mutabiliad = Mutable
+  Ejemplo: {"Nombre":"Juan", "Edad":25}
+'''
