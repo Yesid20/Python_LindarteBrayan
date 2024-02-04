0fbb0c1ccf11c32acc5e0368231b6c7797620c30
Yesid20 <edgarcarloscamilo@gmail.com>
2024-02-04 11:01:33 -0500

    Corrección code

---
+++ Day 6/Ejercicio1_LindarteBr.py
@@ -0,0 +1,19 @@
+## -------------------------------------------
+## ---- Ejercicio 1 ----
+## -------------------------------------------
+
+def negate(num):
+    return -num
+
+def large_num(num):
+    return num > 10000
+
+b = 25
+
+neg_b= negate(b)
+print('b: ', b, 'neg_b: ', neg_b)
+
+big = large_num(b)
+print('b is big: ', big)
+
+# Desarrollado por Brayan Yesid Lindarte Anaya - 1010120365
